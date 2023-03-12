9c626549e2a0e6af06026ca0669af580d426bfa3
Álvaro Berdote <avberdote@hotmail.com>
2023-03-12 19:59:21 +0100

    init repo

---
+++ src/main/java/com/urjc/alumno/alvaro/sbc/SbcApplication.java
@@ -0,0 +1,13 @@
+package com.urjc.alumno.alvaro.sbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urjc/alumno/alvaro/sbc/SbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urjc.alumno.alvaro.sbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
