d78a185463c79beb34f55e6dee953046f9c17280
Álvaro Berdote <avberdote@hotmail.com>
2023-03-15 20:42:41 +0100

    La vida te da sorpresas, sorpresas te da vidaaa

--- src/main/java/com/urjc/alumno/alvaro/sbc/api/common/utils/JenaUtils.java
+++ src/main/java/com/urjc/alumno/alvaro/sbc/api/common/utils/JenaUtils.java
@@ -3,6 +3,9 @@ package com.urjc.alumno.alvaro.sbc.api.common.utils;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFNode;
+
+import java.util.Objects;
 
 public final class JenaUtils {
 
@@ -25,12 +28,29 @@ public final class JenaUtils {
         final Query jenaQuery = QueryFactory.create(query);
         final QueryExecution queryExecution = QueryExecutionFactory.sparqlService(endpoint, jenaQuery);
         final ResultSet resultSet = queryExecution.execSelect();
-        while (resultSet.hasNext()) {
+        resultSet.forEachRemaining(querySolution -> {
+            try {
+                final RDFNode rdfNode = querySolution.get("hasValue");
+                if (Objects.isNull(rdfNode)) {
+                    return;
+                }
+                if (rdfNode.isLiteral()) {
+                    System.out.println(rdfNode.asLiteral().getString());
+                } else if (rdfNode.isURIResource()) {
+                    System.out.println(rdfNode.asResource().getURI());
+                } else {
+                    System.out.println("Que pollas es esto: " + rdfNode.toString());
+                }
+           } catch (Exception e) {
+               System.out.println("wtf mama?");
+           }
+        });
+        /*while (resultSet.hasNext()) {
 
             final QuerySolution next = resultSet.next();
             System.out.println(next.getResource("property").getLocalName());
 
-        }
+        }*/
         return ResultSetFormatter.asText(resultSet);
 
     }
