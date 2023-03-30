f9b3e3ab89a6b4ce7819e70c44b88b5fc6f62f28
Alvaro <avberdote@hotmail.com>
2023-03-30 13:26:48 +0200

    feat(): add html response

---
+++ src/main/java/com/urjc/alumno/alvaro/sbc/api/controller/HtmlController.java
@@ -0,0 +1,29 @@
+package com.urjc.alumno.alvaro.sbc.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/html")
+public class HtmlController {
+
+    @GetMapping(value = "/file", produces = MediaType.TEXT_HTML_VALUE)
+     ResponseEntity<String> getHtml() throws IOException {
+
+        return ResponseEntity.ok(Files.readString(Path.of("src/main/resources/static/index.html")));
+
+    }
+
+}
