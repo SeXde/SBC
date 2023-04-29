9a61c328a3de65446c2795d4c3d87658d141e0e8
Alvaro <avberdote@hotmail.com>
2023-04-29 10:11:26 +0200

    feat: add dockerfile

--- src/main/java/com/urjc/alumno/alvaro/sbc/api/controller/HtmlController.java
+++ src/main/java/com/urjc/alumno/alvaro/sbc/api/controller/HtmlController.java
@@ -2,16 +2,16 @@ package com.urjc.alumno.alvaro.sbc.api.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,11 +19,12 @@ import java.nio.file.Path;
 @RequestMapping
 public class HtmlController {
 
+    @Value("classpath:/static/index.html")
+    private Resource htmlPage;
+
     @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
      ResponseEntity<String> getHtml() throws IOException {
-
-        return ResponseEntity.ok(Files.readString(Path.of("src/main/resources/static/index.html")));
-
+            return ResponseEntity.ok(htmlPage.getContentAsString(StandardCharsets.UTF_8));
     }
 
 }
